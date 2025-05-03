ff1af2d3305b901bc7d1fdf84e3d5246bb01b628
KINGDimsSky <dimasmarcelo2004@gmail.com>
2025-05-04 00:24:02 +0700

    Adding Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,37 @@
 import { ArrowUp } from "lucide-react";
+import { DataFooter } from "@/data/dataFooter";
 
 export default function Footer () {
     return (
-        <div className="flex flex-col items-center h-64">
+        <div className="flex flex-col items-center h-72">
             <div className="flex gap-2 justify-center items-center border-y border-gray-300 h-12 w-full">
-              <div className="flex gap-1 items-center  hover:text-red-500 hover:cursor-pointer">
-                <a href="#">Back to Top</a>
+              <div className="flex gap-1 items-center hover:text-red-500 hover:cursor-pointer">
+                <a href="#" className="py-2">Back to Top</a>
                 <ArrowUp className="w-4 h-4"/>
               </div>
             </div>
-            <div className="flex gap-24 mt-5">
-              <div className="">
-                <h2 className="font-semibold text-gray-800">Beyond Solution</h2>
-                <div className="">
-
-                </div>
+            <div className="flex justify-between px-4 min-w-full md:min-w-6xl">
+              <div className="flex gap-10 mt-5">
+                {DataFooter.map((data) => (
+                  <div key={data.id}>
+                    <h2 className="font-semibold text-gray-900 text-sm ">{data.title}</h2>
+                    <div className="flex flex-col justify-center">
+                      {data.content.map((e, index) => (
+                        <a key={index} href="" className="text-xs mt-1 text-gray-500 hover:text-red-500">{e.title}</a>
+                      ))}
+                   </div>
+                 </div>
+               ))}
               </div>
-              <div className="">
-                <h2>Beyond Experience</h2>
+              <div className="w-24 mt-5">
+                <h2>Social Media</h2>
               </div>
+            </div>
+            <div className="mt-8 pb-6">
+              <div className="min-w-full md:min-w-6xl border-t border-gray-300 mb-4"></div>
+              <h2 className="text-xs text-gray-500 text-center mb-2">C 2025 PT.Toyota Astra Motor </h2>
+              <h2 className="text-[11px] text-gray-500 text-center">All Information Applies to Indonesia Vehicles Only</h2>
+              <h2 className="text-center text-[11px] text-gray-600 mt-1">Privacy Policy  | Legal Cookies | Contact Us </h2>
             </div>
         </div>
     )
--- src/data/dataFooter.ts
+++ src/data/dataFooter.ts
@@ -1,5 +1,11 @@
 export const DataFooter = [
     {id: 1, title: 'Beyond Solution', content: [
-        {type: 'link', content: '', href: ''}
+        {type: 'link', title: 'Toyota Spektakuler', href: ''}, {type: 'link', title: 'Kinto', href: ''}, {type: 'link', title: 'MODA', href: ''}, {type: 'link', title: 'Toyota Financial Service ', href: ''}, {type: 'link', title: 'Get Your Offer', href: ''}, {type: 'link', title: 'Toyota Trade In', href: ''}
+    ]},
+    {id: 2, title: 'Beyond Experience', content: [
+        {type: 'link', title: 'E-Brochure', href: ''}, {type: 'link', title: 'Toyota Interactive Virtual Assistant', href: ''}, {type: 'link', title: 'Toyota Official Store Solution', href: ''}, {type: 'link', title: 'Find Dealer', href: ''}, {type: 'link', title: 'Booking Service', href: ''}
+    ]},
+    {id: 3, title: 'Beyond Technology', content: [
+        {type: 'link', title: 'T-intouch', href: ''}, {type: 'link', title: 'TToyota Safety Sense', href: ''}, {type: 'link', title: 'Android Auto', href: ''}
     ]}
 ]
