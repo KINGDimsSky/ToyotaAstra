811036a2452131a5a9fd0788e2e2317093d6b62c
KINGDimsSky <dimasmarcelo2004@gmail.com>
2025-05-24 00:56:20 +0700

    Disabling ESLINT

--- src/app/berita/page.tsx
+++ src/app/berita/page.tsx
@@ -1,7 +1,22 @@
 import CardNews from "@/components/ClientComponents/CardNews";
 import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
 import { GetNews } from "@/services/index.service";
+import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa";
+
+interface NewsItem {
+  id: number;
+  title: string;
+  category: string;
+  subtitle: string;
+  author: string;
+  date: string;
+  calendarDate: string;
+  slug: string;
+  HeroImage: string;
+  content: string;
+}
+
 export default async function Berita () {
     const news = await GetNews();
     console.log (news.data)
@@ -10,7 +25,7 @@ export default async function Berita () {
         <MaxWidthWrapper className="mt-4 md:mt-8 mb-8 md:mb-12">
           <div className="flex flex-col min-h-screen">
             <div className="flex gap-4 items-center">
-              <a href="/" className="text-gray-600 hover:text-gray-900">Beranda</a>
+              <Link href="/" className="text-gray-600 hover:text-gray-900">Beranda</Link>
               <FaArrowRight className="w-3"/>
               <p className="text-gray-700">Berita</p>
             </div>
@@ -20,7 +35,7 @@ export default async function Berita () {
             <div className="flex justify-center w-full flex-wrap gap-4 mt-6">
               {news.data.length >  0 ? (
                 <div>
-                  {news.data.map((data: any) => (
+                  {news.data.map((data: NewsItem) => (
                     <CardNews key={data.id} title={data.title} category={data.category} calendarDate={data.calendarDate} heroImage={data.HeroImage} slug={data.slug} className=""/>
                   ))}
                 </div>
